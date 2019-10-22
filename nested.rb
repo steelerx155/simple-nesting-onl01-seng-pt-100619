3d8ac3cb3127994d9eeddef73743280e5382ae89
Randy Steele <steelerx155@gmail.com>
2019-10-22 18:26:34 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,9 +81,10 @@ def adding_matz
           :languages => ["C"]
         }
      }
-programmer_hash = { :yukihiro_matsumoto => 
-   :known_for => "Ruby",
-    :languages => ["LISP", "C"]
+programmer_hash[:yukihiro_matsumoto] = {}
+		 programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+		 programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
+		 programmer_hash
 }
 
 programmer_hash[:yukihiro_matsumoto]    
