1a795f3a41d0ec31cc12d5922f4ceda596ceb039
Randy Steele <steelerx155@gmail.com>
2019-10-22 18:00:15 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,9 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-    # languages = programmer_hash[:languages]
-    # programmer_hash.stringify_keys
-    programmer_hash[:dennis_ritchie][:languages]
+        programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
